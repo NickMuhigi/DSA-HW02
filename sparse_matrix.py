821012482b3358e7bf340d19a17b57a8c49f185e
NickMuhigi <144104544+NickMuhigi@users.noreply.github.com>
2024-05-30 18:17:22 +0200

    Sparse-Matrix

--- sparse_matrix.py
+++ sparse_matrix.py
@@ -1,66 +1,65 @@
-# File: /dsa/sparse_matrix/code/src/sparse_matrix.py
 class SparseMatrix:
-    def __init__(self, matrixFilePath=None, numRows=None, numCols=None):
-        if matrixFilePath:
-            self.load_from_file(matrixFilePath)
-        elif numRows and numCols:
-            self.numRows = numRows
-            self.numCols = numCols
+    def __init__(self, file_path=None, rows=None, columns=None):
+        if file_path:
+            self.load_from_txt(file_path)
+        elif rows and columns:
+            self.rows = rows
+            self.columns = columns
             self.matrix = {}
 
-    def load_from_file(self, matrixFilePath):
-        with open(matrixFilePath, 'r') as file:
+    def load_from_txt(self, file_path):
+        with open(file_path, 'r') as file:
             lines = file.readlines()
         
         try:
-            self.numRows = int(lines[0].split('=')[1])
-            self.numCols = int(lines[1].split('=')[1])
+            self.rows = int(lines[0].split('=')[1])
+            self.columns = int(lines[1].split('=')[1])
             self.matrix = {}
 
             for line in lines[2:]:
                 row, col, value = map(int, line.strip().strip('()').split(','))
-                self.set_element(row, col, value)
+                self.set_item(row, col, value)
         except Exception as e:
             raise ValueError("Input file has wrong format") from e
 
-    def get_element(self, currRow, currCol):
+    def get_item(self, currRow, currCol):
         return self.matrix.get((currRow, currCol), 0)
 
-    def set_element(self, currRow, currCol, value):
+    def set_item(self, currRow, currCol, value):
         if value != 0:
             self.matrix[(currRow, currCol)] = value
         elif (currRow, currCol) in self.matrix:
             del self.matrix[(currRow, currCol)]
 
-    def add(self, otherMatrix):
-        result = SparseMatrix(numRows=self.numRows, numCols=self.numCols)
+    def matrix_add(self, otherMatrix):
+        result = SparseMatrix(rows=self.rows, columns=self.columns)
 
         for key in set(self.matrix.keys()).union(otherMatrix.matrix.keys()):
             row, col = key
-            value = self.get_element(row, col) + otherMatrix.get_element(row, col)
-            result.set_element(row, col, value)
+            value = self.get_item(row, col) + otherMatrix.get_item(row, col)
+            result.set_item(row, col, value)
 
         return result
 
-    def subtract(self, otherMatrix):
-        result = SparseMatrix(numRows=self.numRows, numCols=self.numCols)
+    def matrix_subtract(self, otherMatrix):
+        result = SparseMatrix(rows=self.rows, columns=self.columns)
 
         for key in set(self.matrix.keys()).union(otherMatrix.matrix.keys()):
             row, col = key
-            value = self.get_element(row, col) - otherMatrix.get_element(row, col)
-            result.set_element(row, col, value)
+            value = self.get_item(row, col) - otherMatrix.get_item(row, col)
+            result.set_item(row, col, value)
 
         return result
 
-    def multiply(self, otherMatrix):
-        if self.numCols != otherMatrix.numRows:
+    def matrix_multiply(self, otherMatrix):
+        if self.columns != otherMatrix.rows:
             raise ValueError("Number of columns of first matrix must be equal to number of rows of second matrix")
 
-        result = SparseMatrix(numRows=self.numRows, numCols=otherMatrix.numCols)
+        result = SparseMatrix(rows=self.rows, columns=otherMatrix.columns)
 
-        for i in range(self.numRows):
-            for j in range(otherMatrix.numCols):
-                value = sum(self.get_element(i, k) * otherMatrix.get_element(k, j) for k in range(self.numCols))
-                result.set_element(i, j, value)
+        for i in range(self.rows):
+            for j in range(otherMatrix.columns):
+                value = sum(self.get_item(i, k) * otherMatrix.get_item(k, j) for k in range(self.columns))
+                result.set_item(i, j, value)
 
         return result
